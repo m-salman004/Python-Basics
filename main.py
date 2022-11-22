da28d8842d1bc2edd685978e8b2b035e4f3e1cbe
Muhammad Salman <msalmantariq786@gmail.com>
2022-11-22 22:08:49 +0500

    first screen

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
 from kivymd.app import MDApp
 from kivy.uix.screenmanager import Screen,ScreenManager
 from kivy.lang import Builder
+Builder.load_string('''
+<FirstScreen>:
+    name : 'first'
+   
+
+
+''')
