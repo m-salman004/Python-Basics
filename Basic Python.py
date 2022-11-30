81d63fd487f3863ceaac91e204b15f5b870441f4
Muhammad Salman <msalmantariq786@gmail.com>
2022-11-30 22:40:02 +0500

    tuple

--- Basic Python.py
+++ Basic Python.py
@@ -1,45 +1,49 @@
-# Variables in Python
-studentname = "Muhammad Salman"
-print(studentname)
+# # Variables in Python
+# studentname = "Muhammad Salman"
+# print(studentname)
+#
+# classname = "BS Computer Science"
+# print(classname)
+#
+# subjectname = "Android Application Development"
+# print(subjectname)
+# # Data Types
+# # Check Data Type
+# print(type(subjectname))
+# # Operators in Python
+# a = 10
+# b = 50
+# print(a+b)
+# print(a-b)
+# print(a*b)
+# print(a/b)
+#
+# # Rational
+# a = 50
+# b = 20
+# print(a>b)
+# print(a<b)
+# print(a==b)
+# print(a!=b)
+#
+# # Find Word with help of Index
+# strr = "Muhammad Salman"
+# print(strr [11])
+# # Length Of String
+# strr = "Muhammad Salman"
+# print(len(strr))
+# # String Lower and Upper Case
+# print(strr.lower())
+# print(strr.upper())
+# # String Count
+# strr = "This String is a String"
+# print(strr.count("String"))
+# # String Find
+# print(strr.find("String"))
+# # Split String
+# city = ("I Like FSD, Lahore, ISB, Peshawar and Balochistan")
+# print(city.split("ISB"))
 
-classname = "BS Computer Science"
-print(classname)
-
-subjectname = "Android Application Development"
-print(subjectname)
-# Data Types
-# Check Data Type
-print(type(subjectname))
-# Operators in Python
-a = 10
-b = 50
-print(a+b)
-print(a-b)
-print(a*b)
-print(a/b)
-
-# Rational
-a = 50
-b = 20
-print(a>b)
-print(a<b)
-print(a==b)
-print(a!=b)
-
-# Find Word with help of Index
-strr = "Muhammad Salman"
-print(strr [11])
-# Length Of String
-strr = "Muhammad Salman"
-print(len(strr))
-# String Lower and Upper Case
-print(strr.lower())
-print(strr.upper())
-# String Count
-strr = "This String is a String"
-print(strr.count("String"))
-# String Find
-print(strr.find("String"))
-# Split String
-city = ("I Like FSD, Lahore, ISB, Peshawar and Balochistan")
-print(city.split("ISB"))
+# Tuples
+tup = (1, 'a', True, 555.4)
+print(tup)
