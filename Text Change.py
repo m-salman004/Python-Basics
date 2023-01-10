ded8938bff53e2e6585e7ef8909d234dc4f38683
Muhammad Salman <msalmantariq786@gmail.com>
2023-01-11 00:31:02 +0500

    label

--- Text Change.py
+++ Text Change.py
@@ -2,5 +2,12 @@ from kivymd.app import MDApp
 from kivy.uix.screenmanager import Screen,ScreenManager
 from kivy.lang import Builder
 Builder_string = '''
-
+ScreenManager:
+    First:
+<First>:
+    name:"first"
+    MDLabel:
+        id: A
+        text: "Hello World"
+    
 '''
