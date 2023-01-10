ddcefe2cbfe9903dd4aa0db60a2823854587d633
Muhammad Salman <msalmantariq786@gmail.com>
2023-01-11 00:23:27 +0500

    cols and adaptive height

--- Gird Layout.py
+++ Gird Layout.py
@@ -2,3 +2,11 @@ from kivymd.app import MDApp
 from kivy.uix.screenmanager import Screen,ScreenManager
 from kivy.lang import Builder
 Builder_string = '''
+ScreenManager:
+    First:
+<First>:
+    name:"first"
+    ScrollView:
+        MDGridLayout:
+            adaptive_height: True
+            cols:4
